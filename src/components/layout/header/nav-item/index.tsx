fbb96cf5be4de482b11556e83d482e172cdba204
martinalbrnz <martinalbor96@gmail.com>
2023-07-05 01:33:02 -0300

    Delete unused const

--- src/components/layout/header/nav-item/index.tsx
+++ src/components/layout/header/nav-item/index.tsx
@@ -14,7 +14,6 @@ export interface NavItemProps {
 
 const NavItem = (props: NavItemProps) => {
   const id = createUniqueId();
-  const extraClass = props.route.extraClass ?? "";
 
   return (
     <Show
