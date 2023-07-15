81c2424408860b3379dfffe358a1937a193a6253
martinalbrnz <martinalbor96@gmail.com>
2023-07-15 17:53:58 -0300

    Add outcomes model and store

--- src/components/contexts/state.tsx
+++ src/components/contexts/state.tsx
@@ -1,6 +1,7 @@
 import { JSX } from "solid-js";
 import { AuthProvider } from "./auth";
 import { IncomesProvider } from "./incomes";
+import { OutcomesProvider } from "./outcomes";
 import { ThemeProvider } from "./theme";
 
 export interface StateProviderI {
@@ -10,9 +11,11 @@ export interface StateProviderI {
 export const StateProvider = (props: StateProviderI) => {
   return (
     <IncomesProvider incomes={[]}>
-      <AuthProvider auth={undefined}>
-        <ThemeProvider isDark={false}>{props.children}</ThemeProvider>
-      </AuthProvider>
+      <OutcomesProvider outcomes={[]}>
+        <AuthProvider auth={undefined}>
+          <ThemeProvider isDark={false}>{props.children}</ThemeProvider>
+        </AuthProvider>
+      </OutcomesProvider>
     </IncomesProvider>
   );
 };
--- src/models/income.model.ts
+++ src/models/income.model.ts
@@ -1,4 +1,13 @@
 export interface Income {
 	id: string
+	date: Date
+	description?: string
 	amount: number
+	category_id: string // IncomeCategory
+	user_id: string
+}
+
+export interface IncomeCategory {
+	id: string
+	label: string
 }
