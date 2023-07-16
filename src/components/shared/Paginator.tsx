64a67cada2b888aab8dafe9f60ccbbe86e7380be
martinalbrnz <martinalbor96@gmail.com>
2023-07-16 03:22:30 -0300

    Add paginator

---
+++ src/components/shared/Paginator.tsx
@@ -0,0 +1,102 @@
+import {
+  RiArrowsArrowLeftCircleLine,
+  RiArrowsArrowRightCircleLine,
+} from "solid-icons/ri";
+import { Index, Show } from "solid-js";
+
+export interface PaginatorData {
+  page: number;
+  perPage: number;
+  totalItems?: number;
+  totalPages?: number;
+}
+
+export interface PaginatorProps {
+  data: PaginatorData;
+  setPage: (n: number) => void;
+  setPerPage: (n: number) => void;
+}
+
+const Paginator = (props: PaginatorProps) => {
+  const getPages = (page: number, totalPages?: number): number[] => {
+    if (!totalPages) return [];
+
+    let pages = new Array(totalPages)
+      .fill(null, 0, totalPages)
+      .map((_, i) => i + 1);
+
+    if (totalPages > 10) {
+      if (page < 7) {
+        pages = pages.slice(0, 8);
+        pages.push(0);
+        pages.push(totalPages);
+      } else if (page + 5 >= totalPages) {
+        pages = pages.slice(totalPages - 10, totalPages);
+        pages.unshift(0);
+        pages.unshift(1);
+      } else {
+        pages = pages.slice(page - 5, page + 4);
+        pages.unshift(0);
+        pages.unshift(1);
+        pages.push(0);
+        pages.push(totalPages);
+      }
+    }
+
+    return pages;
+  };
+
+  return (
+    <div class="flex gap-2 items-center justify-between bg-gray-200 p-4 rounded shadow">
+      <div class="flex items-center justify-center w-8 h-8">
+        <Show when={props.data.page !== 1}>
+          <RiArrowsArrowLeftCircleLine
+            onclick={() => props.setPage(props.data.page - 1)}
+            class="cursor-pointer text-2xl rounded-full"
+          />
+        </Show>
+      </div>
+
+      <div class="flex flex-1 items-center justify-start gap-1">
+        <Index each={getPages(props.data.page, props.data.totalPages)}>
+          {(page) => (
+            <PageItem data={props.data} i={page()} setPage={props.setPage} />
+          )}
+        </Index>
+      </div>
+
+      <div class="flex items-center justify-center w-8 h-8">
+        <Show when={props.data.page !== props.data.totalPages}>
+          <RiArrowsArrowRightCircleLine
+            onclick={() => props.setPage(props.data.page + 1)}
+            class="cursor-pointer text-2xl rounded-full"
+          />
+        </Show>
+      </div>
+    </div>
+  );
+};
+
+interface PageItemProps {
+  i: number;
+  data: PaginatorData;
+  setPage: (n: number) => void;
+}
+
+const PageItem = (props: PageItemProps) => {
+  return (
+    <Show when={props.i !== 0} fallback={<span class="select-none">...</span>}>
+      <div
+        class="flex items-center justify-center rounded-full border w-8 h-8 transition-all duration-200 select-none cursor-pointer hover:shadow font-medium"
+        classList={{
+          "bg-secondary-300": props.i === props.data.page,
+        }}
+        onclick={() => props.setPage(props.i)}
+      >
+        {props.i}
+      </div>
+    </Show>
+  );
+};
+
+export default Paginator;
--- src/routes/ingresos/index.tsx
+++ src/routes/ingresos/index.tsx
@@ -1,72 +1,106 @@
 import PocketBase from "pocketbase";
-import { For, Show, createEffect, onMount } from "solid-js";
+import { For, Show, createSignal, onMount } from "solid-js";
 import { Title } from "solid-start";
 import { useIncomes } from "~/components/contexts/incomes";
+import Paginator, { PaginatorData } from "~/components/shared/Paginator";
 
 const Ingresos = () => {
   const [incomes, { setIncomes }]: any = useIncomes();
   const pb = new PocketBase(import.meta.env.VITE_API_URL);
+  const [paginatorData, setPaginatorData] = createSignal<PaginatorData>({
+    page: 1,
+    perPage: 10,
+  });
 
-  const fetchRecords = async () => {
-    const records = await pb.collection("income").getFullList({
-      expand: "user_id,category_id",
-      sort: "-date",
+  const fetchRecords = async (pagination: PaginatorData) => {
+    const records = await pb
+      .collection("income")
+      .getList(pagination.page, pagination.perPage, {
+        expand: "user_id,category_id",
+        sort: "-date",
+      });
+
+    const { page, perPage, totalItems, totalPages, items } = records;
+    setPaginatorData({
+      page,
+      perPage,
+      totalItems,
+      totalPages,
     });
-    console.log(records);
-    setIncomes(records);
+    setIncomes(items);
+  };
+
+  const setPage = (page: number) => {
+    if (page === paginatorData().page) return;
+    setPaginatorData({ ...(paginatorData() as PaginatorData), page });
+    fetchRecords({ page, perPage: paginatorData().perPage });
+  };
+
+  const setPerPage = (perPage: number) => {
+    if (perPage === paginatorData().perPage) return;
+    setPaginatorData({ ...(paginatorData() as PaginatorData), perPage });
+    fetchRecords({ page: paginatorData().page, perPage });
   };
 
   onMount(() => {
-    fetchRecords();
+    fetchRecords(paginatorData());
   });
-
-  createEffect(() => {});
 
   return (
     <>
       <Title>Ingresos</Title>
 
-      <Show when={incomes.length > 0}>
-        <div class="flex flex-col gap-2 bg-gray-200 p-4 m-4 rounded shadow">
-          <For each={incomes()}>
-            {(income, i) => (
-              <div class="flex gap-4 items-center justify-between py-2 px-4 bg-white rounded shadow-sm hover:shadow transition-all duration-200">
-                <div class="rounded-full overflow-hidden">
-                  <img
-                    src={pb.getFileUrl(
-                      income.expand.user_id,
-                      income.expand.user_id.avatar,
-                      { thumb: "100x100" }
-                    )}
-                    alt={income.expand.user_id.username}
-                    width={50}
-                    height={50}
-                  />
-                </div>
-                <div class="flex flex-1 flex-col sm:flex-row items-center justify-between">
-                  <div class="flex flex-col gap-1 align-start justify-start sm:justify-center w-full">
-                    <span class="text-sm">
-                      {new Date(income.date).toLocaleString().split(",")[0]}
-                    </span>
-                    <span>
-                      ({income.expand.category_id.label}) {income.description}
-                    </span>
+      <div class="flex flex-col gap-4 m-4">
+        <Show
+          when={incomes().length > 0}
+          fallback={
+            <div class="p-4 m-4">
+              <span>No hay registros</span>
+            </div>
+          }
+        >
+          <div class="flex flex-col gap-2 bg-gray-200 p-4 rounded shadow">
+            <For each={incomes()}>
+              {(income, i) => (
+                <div class="flex gap-4 items-center justify-between py-2 px-4 bg-white rounded shadow-sm hover:shadow transition-all duration-200">
+                  <div class="rounded-full overflow-hidden">
+                    <img
+                      src={pb.getFileUrl(
+                        income.expand.user_id,
+                        income.expand.user_id.avatar,
+                        { thumb: "100x100" }
+                      )}
+                      alt={income.expand.user_id.username}
+                      width={50}
+                      height={50}
+                    />
                   </div>
-                  <div class="flex items-center justify-start sm:justify-between w-full sm:w-28 text-xl ">
-                    <span>$</span>
-                    <span>{income.amount.toFixed(2)}</span>
+                  <div class="flex flex-1 flex-col sm:flex-row items-center justify-between">
+                    <div class="flex flex-col gap-1 align-start justify-start sm:justify-center w-full">
+                      <span class="text-sm">
+                        {new Date(income.date).toLocaleString().split(",")[0]}
+                      </span>
+                      <span>
+                        ({income.expand.category_id.label}) {income.description}
+                      </span>
+                    </div>
+                    <div class="flex items-center justify-start sm:justify-between w-full sm:w-28 text-xl ">
+                      <span>$</span>
+                      <span>{income.amount.toFixed(2)}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
-          </For>
-        </div>
-      </Show>
-      <Show when={incomes.length < 1}>
-        <div class="p-4 m-4">
-          <span>No hay registros</span>
-        </div>
-      </Show>
+              )}
+            </For>
+          </div>
+        </Show>
+
+        <Paginator
+          data={paginatorData()}
+          setPage={setPage}
+          setPerPage={setPerPage}
+        />
+      </div>
     </>
   );
 };
